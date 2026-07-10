38446669459dcb36b3f29e7f358f2a4018053ad7
kaapo <kaapo.pikkarainen@gmail.com>
2026-07-10 15:35:51 +0300

    Inform about config errors rather than silently fail

--- UltimateTeam.cs
+++ UltimateTeam.cs
@@ -1,9 +1,12 @@
+using NLog;
 using ZamboniUltimateTeam.Config;
 
 namespace ZamboniUltimateTeam;
 
 public static class UltimateTeam
 {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
     public static IServerProvider Server;
     public static PackConfig PackConfig { get; private set; }
     public static TournamentConfig TournamentConfig { get; private set; }
@@ -12,26 +15,26 @@ public static class UltimateTeam
     private static string _tournamentConfigPath;
     private static string _hutConfigConfigPath;
 
-    
+
     public static void Initialize(string connectionString, IServerProvider provider)
     {
         UltimateDatabase.ConnectionString = connectionString;
         UltimateDatabase.CreateTables();
         Server = provider;
-        
+
         _packConfigPath = "packs.yml";
         _tournamentConfigPath = "tournaments.yml";
         _hutConfigConfigPath = "hut-config.yml";
         ReloadConfigs();
     }
-    
+
     public static void ReloadConfigs()
     {
         PackConfig = LoadOrCreate(_packConfigPath, ConfigSerializer.DeserializeFile<PackConfig>, ConfigSerializer.SerializeToFile);
         TournamentConfig = LoadOrCreate(_tournamentConfigPath, ConfigSerializer.DeserializeFile<TournamentConfig>, ConfigSerializer.SerializeToFile);
         HutConfig = LoadOrCreate(_hutConfigConfigPath, ConfigSerializer.DeserializeFile<HutConfig>, ConfigSerializer.SerializeToFile);
     }
-    
+
     private static T LoadOrCreate<T>(string path, Func<string, T> deserialize, Action<T, string> serialize) where T : new()
     {
         if (!File.Exists(path))
@@ -40,9 +43,19 @@ public static class UltimateTeam
             serialize(config, path);
             return config;
         }
-        return deserialize(path);
+
+        try
+        {
+            return deserialize(path);
+        }
+        catch (Exception e)
+        {
+            Logger.Fatal("Configuration error in hut configs");
+            Logger.Fatal(e);
+            throw;
+        }
     }
-    
+
     public static uint TimeNowSeconds()
     {
         return (uint)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
